468d8ef13ae6a4d2eaa239aac0911d70c2c05af2
indonesianphiladelphiapausa@gmail.com <indonesianphiladelphiapausa@gmail.com>
2023-09-19 19:50:28 +0000

    Created Main and ServerModule; Edited settings; Deleted Form1

--- client_code/Main/__init__.py
+++ client_code/Main/__init__.py
@@ -1,7 +1,8 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import MainTemplate
 from anvil import *
+import anvil.server
 
-class Form1(Form1Template):
+class Main(MainTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
